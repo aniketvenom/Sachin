82ae5cf913f917f085fa89a5f1497e8f7ad44793
Aniket <144891476+aniketvenom@users.noreply.github.com>
2024-01-23 15:38:43 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+public class ReverseString {
+	
+	public static void main(String args[])
+	{
+		String s1 = "where do you live";
+		String reverse1="",reverse2="";
+		
+		String[] s2 = s1.split(" ");
+		
+		System.out.println(s2.length);
+		
+		for(int i=s2.length-1;i>=0;i--)
+		{
+		
+			reverse1 = reverse1 + s2[i] + " " ;
+		}
+		
+		System.out.println(reverse1);
+		
+		for(int i=s2.length-1;i>=0;i--)
+		{
+			for(int j=s2[i].length()-1;j>=0;j--)
+			{
+				reverse2 += s2[i].charAt(j);
+			}
+			reverse2 += " ";
+		}
+		
+		System.out.println(reverse2);
+	}
+
+}
